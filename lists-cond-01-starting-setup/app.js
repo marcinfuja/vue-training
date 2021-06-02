504f9f9dbcf880e3507c96e9afbead5dce9b3ece
Marcin Fuja <mfuja@gorillagroup.com>
2021-06-02 14:40:34 +0200

    lists 1

---
+++ lists-cond-01-starting-setup/app.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      goal: '',
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.goal)
+    },
+    removeGoal(index) {
+      this.goals.splice(index, 1);
+    } 
+  }
+});
+
+app.mount('#user-goals');
