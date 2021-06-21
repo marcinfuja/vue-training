bd76b75395ad7f65a43f76b95acc1fbcac4211b4
Marcin Fuja <mfuja@gorillagroup.com>
2021-06-21 11:18:35 +0200

    component communication + new assignment

---
+++ cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <section>
+        <active-user :username="user.name" :userage="user.age"></active-user>
+        <user-data @add-user="addUser"></user-data>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            user: {
+                name: 'Marcin',
+                age: '31',   
+            }
+        }
+    },
+    methods: {
+        addUser(changedUser) {
+            const newUser = changedUser;
+            this.user = newUser;
+        } 
+    }
+}
+</script>
---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <form @submit.prevent="submit">
+            <input type="text" v-model="user.name" />
+            <input type="text" v-model="user.age" />
+            <button type="submit">Submit</button>
+        </form>
+    </section>
+</template>
+
+<script>
+export default {
+    emits: ['add-user'],
+    data() {
+        return {
+            user: {
+                name: '',
+                age: '',
+            }
+        }
+    },
+    methods: {
+        submit() {
+            this.$emit('add-user', this.user)
+        }
+    }
+}
+</script>
